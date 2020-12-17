c70b3c304700bdbbf3e5edf3b545a3688b9685b2
anusha <anusha@zipcodes-MacBook-Pro-6.local>
2020-12-17 16:50:51 -0500

    Kafka Phase1

--- phase1/consumer-to-SQL.py
+++ phase1/consumer-to-SQL.py
@@ -1,12 +1,40 @@
 from kafka import KafkaConsumer, TopicPartition
 from json import loads
+import os
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+username = os.getenv('MYSQL_user')
+pswd = os.getenv('MYSQL_pw')
+engine = create_engine('mysql+mysqlconnector://' + username + ":" + pswd + '@localhost/bank')
+Session = sessionmaker()
+Session.configure()
+Base = declarative_base(bind=engine)
+
+
+class Transaction(Base):
+    __tablename__ = 'transaction'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    custid = Column(Integer)
+    type = Column(String(250), nullable=False)
+    date = Column(Integer)
+    amt = Column(Integer)
+
 
 class XactionConsumer:
     def __init__(self):
         self.consumer = KafkaConsumer('bank-customer-events',
-            bootstrap_servers=['localhost:9092'],
-            # auto_offset_reset='earliest',
-            value_deserializer=lambda m: loads(m.decode('ascii')))
+               bootstrap_servers=['localhost:9092'],
+               # auto_offset_reset='earliest',
+               value_deserializer=lambda m: loads(m.decode('ascii')))
+        # self.consumer = KafkaConsumer('bank-customer-events',
+        #                               bootstrap_servers=['localhost:9092'],
+        #                               # auto_offset_reset='earliest',
+        #                               value_deserializer=lambda m: loads(m.decode('ascii')))
         ## These are two python dictionarys
         # Ledger is the one where all the transaction get posted
         self.ledger = {}
@@ -16,8 +44,7 @@ class XactionConsumer:
         # THE PROBLEM is every time we re-run the Consumer, ALL our customer
         # data gets lost!
         # add a way to connect to your database here.
-
-        #Go back to the readme.
+        # Go back to the readme.
 
     def handleMessages(self):
         for message in self.consumer:
@@ -25,6 +52,12 @@ class XactionConsumer:
             print('{} received'.format(message))
             self.ledger[message['custid']] = message
             # add message to the transaction table in your SQL usinf SQLalchemy
+            message_to_sqltable = Transaction(custid=message['custid'], type = message['type'], date=message['date'], amt=message['amt'])
+            # Session = sessionmaker(bind=self.engine)
+            # Session.configure(bind=self.engine)
+            # session = Session()
+            # session.add(message_to_sqltable)
+            # session.commit()
             if message['custid'] not in self.custBalances:
                 self.custBalances[message['custid']] = 0
             if message['type'] == 'dep':
@@ -32,6 +65,12 @@ class XactionConsumer:
             else:
                 self.custBalances[message['custid']] -= message['amt']
             print(self.custBalances)
+            Session = sessionmaker()
+            session = Session()
+            session.add(message_to_sqltable)
+            session.commit()
+
+
 
 if __name__ == "__main__":
     c = XactionConsumer()
